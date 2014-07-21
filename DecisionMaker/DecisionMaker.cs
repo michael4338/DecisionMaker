823a1033d0b95715fbef26ee16e0fefc31c30ad0
Michael Lin <michael4338@hotmail.com>
2014-07-20 22:05:33 -0700

    first commit

---
+++ DecisionMaker/DecisionMaker.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecisionMaker
+{
+    class Decision
+    {
+        public string DecisionName { get; set; }
+        public DateTime DecisionStartTime { get; set; }
+        public DateTime DecisionMadeTime { get; set; }
+
+        public LinkedList<Option> OptionsList
+        {
+            get
+            {
+                return optionsList;
+            }
+        }
+
+        public LinkedList<Criteria> CriteriaList
+        {
+            get
+            {
+                return criteriaList;
+            }
+        }
+
+        private LinkedList<Option> optionsList = null;
+
+        private LinkedList<Criteria> criteriaList = null;
+
+        public Decision(string decisionName)
+        {
+            DecisionName = decisionName;
+            DecisionStartTime = DateTime.Now;
+
+            if (optionsList == null)
+            {
+                optionsList = new LinkedList<Option>();
+            }
+
+            if (criteriaList == null)
+            {
+                criteriaList = new LinkedList<Criteria>();
+            }
+        }
+
+        public Option addOption(string optionName)
+        {
+            if (GivenOptionExists(optionName))
+            {
+                throw new Exception(string.Format("{0} exists already, please give it another name or delete the old one first", optionName));
+            }
+            else
+            {
+                Option option = new Option(optionName);
+                option.UpdateCriteriaInOption(CriteriaList);
+                optionsList.AddLast(option);
+                return option;
+            }
+        }
+
+        public void deleteOption(string optionName)
+        {
+            if (GivenOptionExists(optionName))
+            {
+                optionsList.Remove(optionsList.LastOrDefault(d => d.OptionName.ToLower() == optionName.ToLower()));
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", optionName));
+            }
+        }
+
+        public void updateOption(string oldOptionName, Option newOption)
+        {
+            if (GivenOptionExists(oldOptionName))
+            {
+                optionsList.LastOrDefault(d => d.OptionName.ToLower() == oldOptionName.ToLower()).Description = newOption.Description;
+                optionsList.LastOrDefault(d => d.OptionName.ToLower() == oldOptionName.ToLower()).Link = newOption.Link;
+
+                optionsList.LastOrDefault(d => d.OptionName.ToLower() == oldOptionName.ToLower()).OptionName = newOption.OptionName;
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", oldOptionName));
+            }
+        }
+
+        public Option GetOptionByName(string optionName)
+        {
+            return OptionsList.FirstOrDefault(o => o.OptionName.ToLower() == optionName.ToLower());
+        }
+
+        public void addCriteria(string CriteriaName, int weight = 0)
+        {
+            if (GivenCriteriaExists(CriteriaName))
+            {
+                throw new Exception(string.Format("{0} exists already, please give it another name or delete the old one first", CriteriaName));
+            }
+            else
+            {
+                CriteriaList.AddLast(new Criteria(CriteriaName, weight));
+            }
+
+            UpdateCriteriaInAllOptions();
+        }
+
+        public void deleteCriteria(string CriteriaName)
+        {
+            if (GivenCriteriaExists(CriteriaName))
+            {
+                CriteriaList.Remove(CriteriaList.LastOrDefault(d => d.CriteriaName.ToLower() == CriteriaName.ToLower()));
+
+                UpdateCriteriaInAllOptions();
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", CriteriaName));
+            }
+        }
+
+        public void updateCriteria(string oldCriteriaName, Criteria newCriteria)
+        {
+            if (GivenCriteriaExists(oldCriteriaName))
+            {
+                CriteriaList.LastOrDefault(d => d.CriteriaName.ToLower() == oldCriteriaName.ToLower()).Weight = newCriteria.Weight;
+                // CriteriaList.LastOrDefault(d => d.CriteriaName.ToLower() == oldCriteriaName.ToLower()).Rating = newCriteria.Rating;
+                // CriteriaList.LastOrDefault(d => d.CriteriaName.ToLower() == oldCriteriaName.ToLower()).Value = newCriteria.Value;
+                CriteriaList.LastOrDefault(d => d.CriteriaName.ToLower() == oldCriteriaName.ToLower()).CriteriaName = newCriteria.CriteriaName;
+
+                UpdateCriteriaInAllOptions(oldCriteriaName);
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", oldCriteriaName));
+            }
+        }
+
+        public Criteria GetCriteriaByName(string criteriaName)
+        {
+            return CriteriaList.FirstOrDefault(c => c.CriteriaName.ToLower() == criteriaName.ToLower());
+        }
+
+        public string CalculateAndGetResults()
+        {
+            string output = string.Empty;
+
+            foreach (Option option in OptionsList)
+            {
+                option.CalculateResult();
+                output += option.OptionName + ": " + option.Point + Environment.NewLine;
+            }
+
+            return output;
+        }
+
+        public void ValidateResults(params int[] expectedResults)
+        {
+            string errorMessage = string.Empty;
+
+            if (expectedResults.Length == OptionsList.Count)
+            {
+                int i = 0;
+                foreach (Option option in OptionsList)
+                {
+                    option.CalculateResult();
+                    if (option.Point != expectedResults[i])
+                    {
+                        errorMessage += string.Format("Expected option {0} to have points {1}, but it was {2}\n", option.OptionName, expectedResults[i], option.Point);
+                    }
+
+                    i++;
+                }
+            }
+            else
+            {
+                errorMessage += string.Format("Expected option count to be {0}, but it was {1}\n", expectedResults.Length, OptionsList.Count);
+            }
+
+            if (errorMessage != string.Empty)
+            {
+                throw new Exception(errorMessage);
+            }
+        }
+
+        private bool GivenOptionExists(string optionName)
+        {
+            return optionsList.Any(d => d.OptionName.ToLower() == optionName.ToLower());
+        }
+
+        private bool GivenCriteriaExists(string CriteriaName)
+        {
+            return CriteriaList.Any(d => d.CriteriaName.ToLower() == CriteriaName.ToLower());
+        }
+
+        private void UpdateCriteriaInAllOptions(string updatingCriteriaName = null)
+        {
+            foreach (Option option in optionsList)
+            {
+                option.UpdateCriteriaInOption(CriteriaList, updatingCriteriaName);
+            }
+        }
+    }
+
+    class Option
+    {
+        public string OptionName { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public int Point { get; set; }
+
+        public LinkedList<Criteria> CriteriaList
+        {
+            get
+            {
+                return criteriaList;
+            }
+        }
+
+        public LinkedList<Criteria> criteriaList = null;
+
+        public Option(string name, string description = null, string link = null)
+        {
+            OptionName = name;
+            Description = description;
+            Link = link;
+            Point = 0;
+
+            if (criteriaList == null)
+            {
+                criteriaList = new LinkedList<Criteria>();
+            }
+        }
+
+        public void SetCriteriaRatingAndValue(string criteriaName, int rating, string value = null)
+        {
+            if (GivenCriteriaExists(criteriaName))
+            {
+                Criteria foundCriteria = criteriaList.LastOrDefault(d => d.CriteriaName.ToLower() == criteriaName.ToLower());
+                foundCriteria.Rating = rating;
+                foundCriteria.Value = value;
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", criteriaName));
+            }
+        }
+
+        public void UpdateCriteriaInOption(LinkedList<Criteria> parentCriteriaList, string updatingCriteriaName = null)
+        {
+            foreach (Criteria criteria in parentCriteriaList)
+            {
+                if (GivenCriteriaExists(criteria.CriteriaName))
+                {
+                    criteria.Rating = CriteriaList.First(d => d.CriteriaName.ToLower() == criteria.CriteriaName.ToLower()).Rating;
+                    criteria.Value = CriteriaList.First(d => d.CriteriaName.ToLower() == criteria.CriteriaName.ToLower()).Value;
+                }
+                else if (updatingCriteriaName != null)
+                {
+                    if (GivenCriteriaExists(updatingCriteriaName))
+                    {
+                        criteria.Rating = CriteriaList.First(d => d.CriteriaName.ToLower() == updatingCriteriaName.ToLower()).Rating;
+                        criteria.Value = CriteriaList.First(d => d.CriteriaName.ToLower() == updatingCriteriaName.ToLower()).Value;
+                    }
+                }
+            }
+
+            CriteriaList.Clear();
+            foreach (Criteria criteria in parentCriteriaList)
+            {
+                CriteriaList.AddLast(new Criteria(criteria.CriteriaName, criteria.Weight, criteria.Rating, criteria.Value));
+
+                // Clean parent list for to pretent not touching it anyhow
+                criteria.Rating = 0;
+                criteria.Value = null;
+            }
+        }
+
+        public void CalculateResult()
+        {
+            Point = 0;
+            foreach (Criteria criteria in CriteriaList)
+            {
+                Point += criteria.Weight * criteria.Rating;
+            }
+        }
+
+        private bool GivenCriteriaExists(string criteriaName)
+        {
+            return CriteriaList.Any(d => d.CriteriaName.ToLower() == criteriaName.ToLower());
+        }
+    }
+
+    class Criteria
+    {
+        public string CriteriaName { get; set; }
+        public int Rating { get; set; }
+        public string Value { get; set; }
+        public int Weight { get; set; }
+
+        public Criteria(string name, int weight = 0)
+        {
+            CriteriaName = name;
+            Weight = weight;
+        }
+
+        public Criteria(string name, int weight, int rating, string value)
+        {
+            CriteriaName = name;
+            Weight = weight;
+            Rating = rating;
+            Value = value;
+        }
+    }
+
+    class DecisionMaker
+    {
+        private LinkedList<Decision> decisionsList = null;
+
+        public LinkedList<Decision> DecisionsList
+        {
+            get
+            {
+                return decisionsList;
+            }
+        }
+
+        public DecisionMaker()
+        {
+            if (decisionsList == null)
+            {
+                decisionsList = new LinkedList<Decision>();
+            }
+        }
+
+        public void addDecision(string decisionName)
+        {
+            if (GivenDecisionExists(decisionName))
+            {
+                throw new Exception(string.Format("{0} exists already, please give it another name or delete the old one first", decisionName));
+            }
+            else
+            {
+                decisionsList.AddLast(new Decision(decisionName));
+            }
+        }
+
+        public void deleteDecision(string decisionName)
+        {
+            if (GivenDecisionExists(decisionName))
+            {
+                decisionsList.Remove(decisionsList.LastOrDefault(d => d.DecisionName.ToLower() == decisionName.ToLower()));
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", decisionName));
+            }
+        }
+
+        public void updateDecision(string oldDecisionName, Decision newDecision)
+        {
+            if (GivenDecisionExists(oldDecisionName))
+            {
+                decisionsList.LastOrDefault(d => d.DecisionName.ToLower() == oldDecisionName.ToLower()).DecisionName = newDecision.DecisionName;
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", oldDecisionName));
+            }
+        }
+
+        public Decision GetDecisionByName(string decisionName)
+        {
+            return DecisionsList.FirstOrDefault(d => d.DecisionName.ToLower() == decisionName.ToLower());
+        }
+
+        public void madeDecision(string decisionName)
+        {
+            if (GivenDecisionExists(decisionName))
+            {
+                decisionsList.LastOrDefault(d => d.DecisionName.ToLower() == decisionName.ToLower()).DecisionMadeTime = DateTime.Now;
+            }
+            else
+            {
+                throw new Exception(string.Format("{0} does not exist", decisionName));
+            }
+        }
+
+        private bool GivenDecisionExists(string decisionName)
+        {
+            return decisionsList.Any(d => d.DecisionName.ToLower() == decisionName.ToLower());
+        }
+    }
+}
---
+++ DecisionMaker/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecisionMaker
+{
+    public partial class Form1 : Form
+    {
+        static DecisionMaker decisionMaker = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (decisionMaker == null)
+            {
+                decisionMaker = new DecisionMaker();
+            }
+
+            decisionMaker.addDecision("Buy-A-Car");
+
+            // Basic testing
+            Decision decision1 = decisionMaker.GetDecisionByName("Buy-A-Car");
+            decision1.addCriteria("InExpensive", 100);
+            decision1.addCriteria("Brand", 500);
+            decision1.addCriteria("Service", 50);
+
+            Option option1 = decision1.addOption("BMW");
+            Option option2 = decision1.addOption("Mercedes Benz");
+            Option option3 = decision1.addOption("Toyota");
+
+            option1.SetCriteriaRatingAndValue("InExpensive", 20, "55000");
+            option2.SetCriteriaRatingAndValue("InExpensive", 20, "56000");
+            option3.SetCriteriaRatingAndValue("InExpensive", 100, "24000");
+
+            option1.SetCriteriaRatingAndValue("Brand", 100, "Nice");
+            option2.SetCriteriaRatingAndValue("Brand", 100, "Excellent");
+            option3.SetCriteriaRatingAndValue("Brand", 30, "Soso");
+
+            option1.SetCriteriaRatingAndValue("Service", 80, "Good");
+            option2.SetCriteriaRatingAndValue("Service", 60, "Bad");
+            option3.SetCriteriaRatingAndValue("Service", 90, "Great");
+
+            decision1.ValidateResults(56000, 55000, 29500);
+            MessageBox.Show("Buy-A-Car points: \n" + decision1.CalculateAndGetResults());
+
+            // Add new decision
+            decisionMaker.addDecision("Buy-A-House");
+            Decision decision2 = decisionMaker.GetDecisionByName("Buy-A-House");
+
+            decision2.addCriteria("Location", 500);
+            decision2.addCriteria("Condition", 200);
+            decision2.addCriteria("Signed School", 100);
+            decision2.addCriteria("Price", 500);
+
+            Option option21 = decision2.addOption("MountainView Apmt");
+            Option option22 = decision2.addOption("SanJose SFH");
+            Option option23 = decision2.addOption("Dublin TownHouse");
+
+            option21.SetCriteriaRatingAndValue("Location", 200, "Close to nice north");
+            option22.SetCriteriaRatingAndValue("Location", 100, "Not great");
+            option23.SetCriteriaRatingAndValue("Location", 60, "Too far away");
+
+            option21.SetCriteriaRatingAndValue("Condition", 20, "Poor");
+            option22.SetCriteriaRatingAndValue("Condition", 100, "Neat");
+            option23.SetCriteriaRatingAndValue("Condition", 200, "Great");
+
+            option21.SetCriteriaRatingAndValue("Signed School", 90, "9P");
+            option22.SetCriteriaRatingAndValue("Signed School", 20, "5P");
+            option23.SetCriteriaRatingAndValue("Signed School", 100, "10P");
+
+            option21.SetCriteriaRatingAndValue("Price", 20, "Expensive");
+            option22.SetCriteriaRatingAndValue("Price", 100, "Affordable");
+            option23.SetCriteriaRatingAndValue("Price", 120, "Better");
+
+            decision2.ValidateResults(123000, 122000, 140000);
+            MessageBox.Show("Buy-A-House points: \n" + decision2.CalculateAndGetResults());
+
+            // Test update&delete options
+            option21.OptionName = @"MountainView Condo";
+            //decision2.updateOption("MountainView Apmt", option21);
+            MessageBox.Show("Update option name: \n" + decision2.CalculateAndGetResults());
+
+            decision2.deleteOption("MountainView Condo");
+            decision2.ValidateResults(122000, 140000);
+            MessageBox.Show("Delete option: \n" + decision2.CalculateAndGetResults());
+
+            // Test update&delete criteria
+            Criteria newCriteria1 = new Criteria("How is the school", 10);
+            decision2.updateCriteria("Signed School", newCriteria1);
+            decision2.ValidateResults(120200, 131000);
+            MessageBox.Show("Update Criteria name and weight: \n" + decision2.CalculateAndGetResults());
+
+            decision2.deleteCriteria("How is the school");
+            decision2.ValidateResults(120000, 130000);
+            MessageBox.Show("Delete criteria: \n" + decision2.CalculateAndGetResults());
+
+            // Test add options
+            Option option24 = decision2.addOption("Sunnyvale Townhouse");
+            option24.SetCriteriaRatingAndValue("Location", 100, "Neat");
+            // option24.SetCriteriaRatingAndValue("Price", 80, "Bad");
+            option24.SetCriteriaRatingAndValue("Condition", 50, "Soso");
+            decision2.ValidateResults(120000, 130000, 60000);
+            MessageBox.Show("Add an option: \n" + decision2.CalculateAndGetResults());
+
+            // Test add criteria
+            decision2.addCriteria("View", 300);
+            decision2.ValidateResults(120000, 130000, 60000);
+            MessageBox.Show("Add a criteria with all rating as 0: \n" + decision2.CalculateAndGetResults());
+
+            decision2.GetOptionByName("SanJose SFH").SetCriteriaRatingAndValue("View", 200, "mountain view");
+            decision2.GetOptionByName("Dublin TownHouse").SetCriteriaRatingAndValue("View", 300, "Beautiful mountain view");
+            decision2.GetOptionByName("Sunnyvale Townhouse").SetCriteriaRatingAndValue("View", 30, "Neighbour view");
+            decision2.ValidateResults(180000, 220000, 69000);
+            MessageBox.Show("Add a criteria with setting ratings to options: \n" + decision2.CalculateAndGetResults());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
